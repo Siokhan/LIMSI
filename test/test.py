41bb4db1c6c508cf4992f3f994525c2b9043282e
Siokhan <siokhank@gmail.com>
2017-07-06 16:12:27 +0200

    switched to using panda, can now print selected values in csv file

--- test/test.py
+++ test/test.py
@@ -1,5 +1,7 @@
-import csv 
-with open ('export.csv', 'rb') as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        print row
+import pandas as pd
+from pandas import DataFrame
+
+
+df = pd.read_csv('export.csv', encoding = "ISO-8859-1", index_col='ID')
+df2 = df[['filename', 'track', 'start', 'end', 'duration', 'Value']]
+print(df2)
