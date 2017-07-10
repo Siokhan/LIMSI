be517120ba81c064e88543830ddf0f9d1f8522fb
Siokhan <siokhank@gmail.com>
2017-07-10 11:26:16 +0200

    Made a fake data table to compare to

--- test/test.py
+++ test/test.py
@@ -1,7 +1,11 @@
 import pandas as pd
 from pandas import DataFrame
 
-
 df = pd.read_csv('export.csv', encoding = "ISO-8859-1", index_col='ID')
+testdf = pd.read_csv('testdata.csv', encoding = "ISO-8859-1", index_col='ID')
+
 df2 = df[['filename', 'track', 'start', 'end', 'duration', 'Value']]
-print(df2)
+testdf2 = testdf[['filename', 'track', 'start', 'end', 'duration', 'Value']]
+
+print(df2.head())
+print(testdf2.head())
