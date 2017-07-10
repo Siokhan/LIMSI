5f69d914fba9b9b7d2cad07c570c010df43e2ac7
Siokhan <siokhank@gmail.com>
2017-07-10 15:38:09 +0200

    Manage to compare 2 csv files and add a row that displays whether or not there are differences, I get a warning however, need to figure out before moving forward

--- test/test.py
+++ test/test.py
@@ -1,11 +1,19 @@
 import pandas as pd
+import numpy as np
 from pandas import DataFrame
 
 df = pd.read_csv('export.csv', encoding = "ISO-8859-1", index_col='ID')
 testdf = pd.read_csv('testdata.csv', encoding = "ISO-8859-1", index_col='ID')
 
-df2 = df[['filename', 'track', 'start', 'end', 'duration', 'Value']]
-testdf2 = testdf[['filename', 'track', 'start', 'end', 'duration', 'Value']]
+df2 = df.loc[:,('filename', 'track', 'start', 'end', 'duration', 'Value')]
+testdf2 = testdf.loc[:,('filename', 'track', 'start', 'end', 'duration', 'Value')]
 
-print(df2.head())
-print(testdf2.head())
+df3 = df2.loc[(df2['track'] == "Gaze")]
+testdf3 = testdf2.loc[(testdf2['track'] == "Gaze")]
+
+print(df3.head())
+print(testdf3) 
+
+testdf3['check'] = np.where(testdf3.Value == df3.Value, 'valid', 'invalid')
+
+print(testdf3)
